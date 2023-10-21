53e4ea3a902e9a094259deefc3a50adcb16a4221
Pavel Krapivin <pavelkrapivin@Pavels-Air.attlocal.net>
2023-10-20 20:15:39 -0400

    playSound

---
+++ background.js
@@ -0,0 +1,6 @@
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.action === 'popupReady') {
+//     chrome.runtime.sendMessage({ action: 'backgroundReady' });
+//     playRandom();
+//   }
+// });
