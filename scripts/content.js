ba743f54a374cd65eec2739d1f8c73a10ea77ee6
Ross Terker <ross.terker@gmail.com>
2023-10-21 12:22:28 -0400

    implemented farting sound function on key press

--- scripts/content.js
+++ scripts/content.js
@@ -2,39 +2,45 @@ chrome.runtime.onMessage.addListener(messageReceived);
 
 function messageReceived(message, sender, sendResponse) {
   if (message.onOrOff === 1) {
-    alert('ON: Event Listener Added')
+    // alert('ON: Event Listener Added')
     document.addEventListener('keydown', activateOnKeyStroke);
   }
   if (message.onOrOff === -1) {
     document.removeEventListener('keydown', activateOnKeyStroke);
-    alert('OFF: Event Listener Removed');
+    // alert('OFF: Event Listener Removed');
   }
   sendResponse(message.onOrOff *= -1);
 }
 
 function activateOnKeyStroke(event) {
   if (event.key === 'A' || event.key === 'a') {
-    document.body.style.backgroundColor = "orange";
-    alert('Key Pressed!')
+    function playRandom() {
+      const audio = new Audio();
+      const randomIndex = Math.floor(Math.random() * soundFiles.length);
+      const selectedSound = soundFiles[randomIndex];
+      audio.src = chrome.runtime.getURL(selectedSound); //gets URL of the sound within extension, ensures that extension can access the file
+      audio.play();
+    }
+    playRandom()
   }
 }
 
 
-// const soundFiles = [
-//   'sounds/fart6.mp3',
-//   'sounds/fart2.mp3',
-//   'sounds/fart3.mp3',
-//   'sounds/fart4.mp3',
-//   'sounds/fart5.mp3',
-//   'sounds/fart7.mp3',
-//   'sounds/fart8.mp3',
-//   'sounds/fart9.mp3',
-//   'sounds/fart10.mp3',
-//   'sounds/fart11.mp3',
-//   'sounds/fart12.mp3',
-//   'sounds/fart13.mp3',
-//   'sounds/fart14.mp3',
-// ];
+const soundFiles = [
+  'sounds/fart6.mp3',
+  'sounds/fart2.mp3',
+  'sounds/fart3.mp3',
+  'sounds/fart4.mp3',
+  'sounds/fart5.mp3',
+  'sounds/fart7.mp3',
+  'sounds/fart8.mp3',
+  'sounds/fart9.mp3',
+  'sounds/fart10.mp3',
+  'sounds/fart11.mp3',
+  'sounds/fart12.mp3',
+  'sounds/fart13.mp3',
+  'sounds/fart14.mp3',
+];
 
 // soundURLs = soundFiles.map(el => {
 //     return chrome.runtime.getURL(el);
@@ -44,25 +50,25 @@ function activateOnKeyStroke(event) {
 
     
 // const audio = new Audio();
-// console.log(audio);
-// audio.play();
 
-const audioElement = document.createElement('audio');
-audioElement.setAttribute('id', 'audio-element')
-const soundURL = chrome.runtime.getURL('sounds/fart6.mp3');
-// const sound = new Audio(soundURL);
-// sound.play()
-audioElement.setAttribute('src', soundURL)
-audioElement.autoplay = true;
-const aTag = document.createElement('a');
-aTag.setAttribute('href', soundURL);
-audioElement.appendChild(aTag);
-document.head.appendChild(audioElement);
+
+// const audioElement = document.createElement('audio');
+// audioElement.setAttribute('id', 'audio-element')
+// const soundURL = chrome.runtime.getURL('sounds/fart6.mp3');
+// // const sound = new Audio(soundURL);
+// // sound.play()
+// audioElement.setAttribute('src', soundURL)
+// audioElement.autoplay = true;
+// const aTag = document.createElement('a');
+// aTag.setAttribute('href', soundURL);
+// audioElement.appendChild(aTag);
+// document.head.appendChild(audioElement);
 // audioElement.play();
 
 
-//randomize played sounds
+// randomize played sounds
 // function playRandom() {
+//     const audio = new Audio();
 //     const randomIndex = Math.floor(Math.random() * soundFiles.length);
 //     const selectedSound = soundFiles[randomIndex];
 //     audio.src = chrome.runtime.getURL(selectedSound); //gets URL of the sound within extension, ensures that extension can access the file
